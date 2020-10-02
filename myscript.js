06c0264d9ef677aa6b5bd77f01c27b40eb5ffe32
Subham Karmakar <50767716+SubhamKarmakar24@users.noreply.github.com>
2020-10-03 00:24:08 +0530

    Fixed alternate X and O

--- myscript.js
+++ myscript.js
@@ -1,5 +1,3 @@
-
-
 // Restart Game Button
 console.log('connected');
 var restart = document.querySelector('#b');
@@ -7,32 +5,53 @@ var restart = document.querySelector('#b');
 // Grabs all the squares
 var squares = document.querySelectorAll('td');
 
+//turn to determine whether X or O is added next
+var turn = 1;
+
 // Clear all the squares
-function clearBoard() {
+function clearBoard()
+{
 	// body...
-	for (var i = 0; i < squares.length; i++) {
-		squares[i].textContent = '';
+     for (var i = 0; i < squares.length; i++)
+     {
+		squares[i].textContent = '\u00A0';
 	}
-	
+	turn = 1;
 }
 
 restart.addEventListener('click',clearBoard);
 
 // Check the square marker
 
-function changemarker(){
-	if(this.textContent===''){
-		this.textContent='X';
-	}else if(this.textContent==='X'){
-		this.textContent='O';
-	}else{
-		this.textContent='';
-	}
+function changemarker()
+{
+     if((this.textContent!='X')&&(this.textContent!='O'))
+     {
+          if(turn%2!=0)
+          {
+               this.textContent='X';
+               turn++;
+          }
+          else
+          {
+               this.textContent='O';
+               turn++; 
+          }
+     }
 }
 
-for(let i=0;i<squares.length;i++){
-	squares[i].addEventListener('click',changemarker); 
+for(let i=0;i<squares.length;i++)
+{
+     squares[i].addEventListener('click',changemarker); 
+     checkWinner;
 }
 
+
+//checking winner
+function checkWinner()
+{
+     if(squares[0]=='X')
+          alert("Won");
+}
 
 // For loop to add event listeners to all the sqquares
