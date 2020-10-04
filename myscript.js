91b6c8eb50ee16b5de7b23090fc32a7ead325fa7
Subham Karmakar <50767716+SubhamKarmakar24@users.noreply.github.com>
2020-10-04 18:59:30 +0530

    Winning and Draw logic corrected

--- myscript.js
+++ myscript.js
@@ -5,7 +5,7 @@ var restart = document.querySelector('#b');
 // Grabs all the squares
 var squares = document.querySelectorAll('td');
 
-//turn to determine whether X or O is added next
+//turn to determine whether ❌ or ⭕ is added next
 var turn = 1;
 
 // Clear all the squares
@@ -25,7 +25,7 @@ restart.addEventListener('click',clearBoard);
 
 function changemarker()
 {
-     if((this.textContent!='X')&&(this.textContent!='O'))
+     if((this.textContent!='❌')&&(this.textContent!='⭕'))
      {
           if(turn%2!=0)
           {
@@ -39,6 +39,7 @@ function changemarker()
           }
      }
      checkWinner();
+     checkDraw();
 }
 
 for(let i=0;i<squares.length;i++)
@@ -50,85 +51,104 @@ for(let i=0;i<squares.length;i++)
 //checking the winner 
 function checkWinner()
 {
-     if((squares[0].textContent=='X')&&(squares[1].textContent=='X')&&(squares[2].textContent=='X'))
+     if((squares[0].textContent=='❌')&&(squares[1].textContent=='❌')&&(squares[2].textContent=='❌'))
      {
           alert("Player 1 Won");
           clearBoard();
      }
           
-     else if((squares[0].textContent=='O')&&(squares[1].textContent=='O')&&(squares[2].textContent=='O'))
+     else if((squares[0].textContent=='⭕')&&(squares[1].textContent=='⭕')&&(squares[2].textContent=='⭕'))
      {
           alert("Player 2 Won");
           clearBoard();
      }
-     else if((squares[3].textContent=='X')&&(squares[4].textContent=='X')&&(squares[5].textContent=='X'))
+     else if((squares[3].textContent=='❌')&&(squares[4].textContent=='❌')&&(squares[5].textContent=='❌'))
      {
           alert("Player 1 Won");
           clearBoard();    
      }
-     else if((squares[3].textContent=='O')&&(squares[4].textContent=='O')&&(squares[5].textContent=='O'))
+     else if((squares[3].textContent=='⭕')&&(squares[4].textContent=='⭕')&&(squares[5].textContent=='⭕'))
      {
           alert("Player 2 Won");
           clearBoard();
      }
-     else if((squares[6].textContent=='X')&&(squares[7].textContent=='X')&&(squares[8].textContent=='X'))
+     else if((squares[6].textContent=='❌')&&(squares[7].textContent=='❌')&&(squares[8].textContent=='❌'))
      {
           alert("Player 1 Won");
           clearBoard();
      }
-     else if((squares[6].textContent=='O')&&(squares[7].textContent=='O')&&(squares[8].textContent=='O'))
+     else if((squares[6].textContent=='⭕')&&(squares[7].textContent=='⭕')&&(squares[8].textContent=='⭕'))
      {
           alert("Player 2 Won");
           clearBoard();
      }
-     else if((squares[0].textContent=='X')&&(squares[3].textContent=='X')&&(squares[6].textContent=='X'))
+     else if((squares[0].textContent=='❌')&&(squares[3].textContent=='❌')&&(squares[6].textContent=='❌'))
      {
           alert("Player 1 Won");
           clearBoard();
      }
-     else if((squares[0].textContent=='O')&&(squares[3].textContent=='O')&&(squares[6].textContent=='O'))
+     else if((squares[0].textContent=='⭕')&&(squares[3].textContent=='⭕')&&(squares[6].textContent=='⭕'))
      {
           alert("Player 2 Won");
           clearBoard();
      }
-     else if((squares[1].textContent=='X')&&(squares[4].textContent=='X')&&(squares[7].textContent=='X'))
+     else if((squares[1].textContent=='❌')&&(squares[4].textContent=='❌')&&(squares[7].textContent=='❌'))
      {
           alert("Player 1 Won");
           clearBoard();
      }
-     else if((squares[1].textContent=='O')&&(squares[4].textContent=='O')&&(squares[7].textContent=='O'))
+     else if((squares[1].textContent=='⭕')&&(squares[4].textContent=='⭕')&&(squares[7].textContent=='⭕'))
      {
           alert("Player 2 Won");
           clearBoard();
      }
-     else if((squares[2].textContent=='X')&&(squares[5].textContent=='X')&&(squares[8].textContent=='X'))
+     else if((squares[2].textContent=='❌')&&(squares[5].textContent=='❌')&&(squares[8].textContent=='❌'))
      {
           alert("Player 1 Won");
           clearBoard();
      }
-     else if((squares[2].textContent=='O')&&(squares[5].textContent=='O')&&(squares[8].textContent=='O'))
+     else if((squares[2].textContent=='⭕')&&(squares[5].textContent=='⭕')&&(squares[8].textContent=='⭕'))
      {
           alert("Player 2 Won");
           clearBoard();
      }
-     else if((squares[0].textContent=='X')&&(squares[4].textContent=='X')&&(squares[8].textContent=='X'))
+     else if((squares[0].textContent=='❌')&&(squares[4].textContent=='❌')&&(squares[8].textContent=='❌'))
      {
           alert("Player 1 Won");
           clearBoard();    
      }
-     else if((squares[0].textContent=='O')&&(squares[4].textContent=='O')&&(squares[8].textContent=='O'))
+     else if((squares[0].textContent=='⭕')&&(squares[4].textContent=='⭕')&&(squares[8].textContent=='⭕'))
      {
           alert("Player 2 Won");
           clearBoard();
      }
-     else if((squares[2].textContent=='X')&&(squares[4].textContent=='X')&&(squares[6].textContent=='X'))
+     else if((squares[2].textContent=='❌')&&(squares[4].textContent=='❌')&&(squares[6].textContent=='❌'))
      {
           alert("Player 1 Won");
           clearBoard();
      }
-     else if((squares[2].textContent=='O')&&(squares[4].textContent=='O')&&(squares[6].textContent=='O'))
+     else if((squares[2].textContent=='⭕')&&(squares[4].textContent=='⭕')&&(squares[6].textContent=='⭕'))
      {
           alert("Player 2 Won");
+          clearBoard();
+     }
+}
+
+let draw = -1;
+
+function checkDraw()
+{
+     draw = 0;
+     for(let k=0;k<squares.length;k++)
+     {
+          if((squares[k].textContent=='❌')||(squares[k].textContent=='⭕'))
+          {
+               draw++;
+          }
+     }
+     if(draw==9)
+     {
+          alert("It's a Draw");
           clearBoard();
      }
 }
